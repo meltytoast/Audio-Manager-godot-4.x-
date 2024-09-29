31f83aebd7e1bfe04ccb1d75744532d859d21b16
kimberly ˚ʚ♡ɞ˚ <Kimberlyludvik@gmail.com>
2024-09-29 06:51:14 +0200

    Delete Global.gd

--- Global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# Global values
-
-var score_death = 0  # tracks how many times you have finished the game OR quit to menu
-
-var ghosta_relationship_pts = 0 # 
-var ghostb_relationship_pts = 0 # 
-var ghostc_relationship_pts = 0 # print these at end of Cafe Mode ! 
