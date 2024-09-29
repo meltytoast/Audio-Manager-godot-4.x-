40bf927cd671fd0fa64e48144c62c328d94463cb
kimberly ˚ʚ♡ɞ˚ <Kimberlyludvik@gmail.com>
2024-09-29 06:58:15 +0200

    Update signal_manager.gd

--- signal_manager.gd
+++ signal_manager.gd
@@ -1,9 +1,9 @@
 extends Node
 
-# signal hub ! 
-# global script 
+# signal hub 
+# handles SFX & BGM playback, connects emitted signals to audio_manager.gd
 
 signal outer_waves_bgm # BGM for lighthouse exterior
 signal say_hi # hub for say_hi, connects from CAFE MODE . tscn 
-
+signal 
 
